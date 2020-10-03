f2f9f45d44f9f3c0a1d425990f67677dff8c7c04
Tentapoulpe <arthur.manoux@wanadoo.fr>
2020-10-03 18:13:14 +0200

    wip main menu + wip pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -24,5 +25,16 @@ public class GameManager : MonoBehaviour
     private void Update()
     {
         testing?.Invoke() ;
+    }
+
+    [SerializeField] private string m_firstLevelName;
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(m_firstLevelName);
     }
 }
---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    public static UiManager Instance { get; private set; }
+    [SerializeField] GameObject m_pauseMenu;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    public void ShowPauseMenu(bool value)
+    {
+        m_pauseMenu.SetActive(value);
+    }
+}
