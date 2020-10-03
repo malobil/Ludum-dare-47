6999829fefed0d86456ed3aa8672ea626941cdfb
Tentapoulpe <arthur.manoux@wanadoo.fr>
2020-10-04 00:47:19 +0200

    animation button

--- Assets/Scripts/Interactions/InteractableObject.cs
+++ Assets/Scripts/Interactions/InteractableObject.cs
@@ -6,11 +6,17 @@ using UnityEngine.Events;
 public class InteractableObject : MonoBehaviour, InteractableElement
 {
     [SerializeField] private List<UnityEvent> m_interactEvent = new List<UnityEvent>() ;
+    [SerializeField] private Animator m_animator = null;
+    [SerializeField] private string m_triggerAnimationName = string.Empty;
     public virtual void Interact()
     {
         for(int i = 0; i < m_interactEvent.Count ; i++)
         {
             m_interactEvent[i]?.Invoke();
+        }
+        if(m_animator!= null)
+        {
+            m_animator.SetTrigger(m_triggerAnimationName);
         }
     }
 
