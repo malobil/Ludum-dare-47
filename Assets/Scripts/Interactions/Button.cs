e49656375068684c902653be347c2bc3d04ac434
Tentapoulpe <arthur.manoux@wanadoo.fr>
2020-10-03 17:19:08 +0200

    start golden nugget

---
+++ Assets/Scripts/Interactions/Button.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour, InteractableElementOnShot
+{
+    public void InteractOnShot()
+    {
+        Debug.Log("InteractOnShot");
+    }
+}
---
+++ Assets/Scripts/Player/PlayerShot.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShot : MonoBehaviour
+{
+    [SerializeField] private GameObject m_goldenNugget;
+}
