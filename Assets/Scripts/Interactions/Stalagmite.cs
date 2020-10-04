39e0ddbfaa0cb423a896f8ae5b89982d677c7959
Tentapoulpe <arthur.manoux@wanadoo.fr>
2020-10-04 12:51:00 +0200

    wip stalagmite

---
+++ Assets/Scripts/Interactions/Stalagmite.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stalagmite : InteractableObject
+{
+    public override void Interact()
+    {
+        base.Interact();
+    }
+}
